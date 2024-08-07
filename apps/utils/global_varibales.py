5a7713255ea84000f8e63053b4e25101252960f8
yibrahim <yasir.ibrahim@gmail.com>
2024-08-07 15:25:38 +0500

    global variable to handle mandatory first user role claude limitation

--- apps/utils/global_varibales.py
+++ apps/utils/global_varibales.py
@@ -1,3 +1,6 @@
 class CollectionVariables:
     MY_COLLECTION = "My Collection"
     SHARED_WITH_ME_COLLECTION = "Shared With Me"
+
+class AIModelVariables:
+    CLAUDE_USER_DUMMY_MESSAGE = {"role": "user", "content": "This is a conversation between user and assistant"}
