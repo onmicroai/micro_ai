275bef8c7945a60fc8ef71b9991b3d8795335b3a
yibrahim <yasir.ibrahim@gmail.com>
2024-08-08 13:02:04 +0500

    modified previous ai-model variable and added new

--- apps/utils/global_varibales.py
+++ apps/utils/global_varibales.py
@@ -3,4 +3,5 @@ class CollectionVariables:
     SHARED_WITH_ME_COLLECTION = "Shared With Me"
 
 class AIModelVariables:
-    CLAUDE_USER_DUMMY_MESSAGE = {"role": "user", "content": "This is a conversation between user and assistant"}
+    CLAUDE_USER_DUMMY_MESSAGE_FIRST = {"role": "user", "content": "This is a conversation between user and assistant"}
+    CLAUDE_USER_DUMMY_MESSAGE_LAST = {"role": "user", "content": "your thoughts on this"}
