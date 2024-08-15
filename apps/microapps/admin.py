2ba881150c3cdc871ad4cbedc38271cadc291725
yibrahim <yasir.ibrahim@gmail.com>
2024-08-15 12:55:04 +0500

    django-admin registeration for model ai-model-config

--- apps/microapps/admin.py
+++ apps/microapps/admin.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
-from .models import MicroAppUserJoin, Microapp, Asset, AssetsMaJoin, KnowledgeBase, Run
+from .models import AiModelConfig, MicroAppUserJoin, Microapp, Asset, AssetsMaJoin, KnowledgeBase, Run
 
 # Register your models here.
 
 admin.site.register(MicroAppUserJoin)
 admin.site.register(Microapp)
-# admin.site.register(Asset)
-# admin.site.register(AssetsMaJoin)
-# admin.site.register(KnowledgeBase)
+admin.site.register(AiModelConfig)
 admin.site.register(Run)
