389187e1bf2d78cee8058c7a92ee6dac06b6e310
yibrahim <yasir.ibrahim@gmail.com>
2024-08-16 14:35:02 +0500

    import model statement updated for AIModelConfig

--- apps/microapps/admin.py
+++ apps/microapps/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import AiModelConfig, MicroAppUserJoin, Microapp, Asset, AssetsMaJoin, KnowledgeBase, Run
+from .models import AIModelConfig, MicroAppUserJoin, Microapp, Asset, AssetsMaJoin, KnowledgeBase, Run
 
 # Register your models here.
 
 admin.site.register(MicroAppUserJoin)
 admin.site.register(Microapp)
-admin.site.register(AiModelConfig)
+admin.site.register(AIModelConfig)
 admin.site.register(Run)
