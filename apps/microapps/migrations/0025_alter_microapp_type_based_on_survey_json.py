3bc0c05c686e64d7e9b0510a34c2d5b3e76b3db4
brnikita <branikita@gmail.com>
2024-09-30 15:06:08 +0300

    Fix: Updated migration for the microapp type field

--- apps/microapps/migrations/0025_alter_microapp_type_based_on_survey_json.py
+++ apps/microapps/migrations/0025_alter_microapp_type_based_on_survey_json.py
@@ -2,19 +2,27 @@
 
 
 from django.db import migrations
+import json
 
 
 def populate_type_field(apps, schema_editor):
     Microapp = apps.get_model('microapps', 'Microapp')
     for microapp in Microapp.objects.all():
-        app_json = microapp.app_json or {}
-        # Get privacySettings from app_json
-        privacy_settings = app_json.get('privacySettings')
-        if privacy_settings:
-            # Populate the type field with privacySettings value
-            microapp.type = privacy_settings
-        else:
+        try:
+            # Try to parse app_json as JSON if it's a string
+            app_json = json.loads(microapp.app_json) if isinstance(microapp.app_json, str) else (microapp.app_json or {})
+            
+            # Get privacySettings from app_json
+            privacy_settings = app_json.get('privacySettings')
+            if privacy_settings:
+                # Populate the type field with privacySettings value
+                microapp.type = privacy_settings
+            else:
+                microapp.type = 'Private'
+        except json.JSONDecodeError:
+            # If JSON parsing fails, set type to 'Private'
             microapp.type = 'Private'
+        
         microapp.save(update_fields=['type'])
 
 class Migration(migrations.Migration):
