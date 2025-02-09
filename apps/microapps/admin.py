13c1d06953c6386fa16073750df2da17abbd9241
John Swope <john@johnswope.com>
2025-02-09 13:17:40 -0500

    Remove references to AI Model Configs

--- apps/microapps/admin.py
+++ apps/microapps/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import AIModelConfig, MicroAppUserJoin, Microapp, Run
+from .models import MicroAppUserJoin, Microapp, Run
 
 # Register your models here.
 
 admin.site.register(MicroAppUserJoin)
 admin.site.register(Microapp)
-admin.site.register(AIModelConfig)
 admin.site.register(Run)
