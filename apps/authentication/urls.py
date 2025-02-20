d325a8ca240b8a38f68d5d21ba6c92311e70b5e5
brnikita <branikita@gmail.com>
2025-02-20 18:00:05 +0300

    feat: #65 Add custom password reset views and URL routes

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -3,7 +3,7 @@
 from dj_rest_auth.jwt_auth import get_refresh_view
 from dj_rest_auth.registration.views import RegisterView
 from dj_rest_auth.views import UserDetailsView, PasswordChangeView
-from django.urls import path
+from django.urls import path, re_path
 from rest_framework_simplejwt.views import TokenVerifyView
 from . import api_views
 from . import views
@@ -17,6 +17,12 @@ urlpatterns = [
     path("logout/", api_views.APICustomLogoutView.as_view(), name="rest_logout"),
     path("user/", views.CustomUserDetailsView.as_view(), name="rest_user_details"),
     path("password/change/", PasswordChangeView.as_view(), name="change_password"),
+    path("password/reset/", views.CustomPasswordResetView.as_view(), name="password_reset"),
+    re_path(
+        r"^password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,32})/$",
+        views.CustomPasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("token/refresh/", get_refresh_view().as_view(), name="token_refresh"),
 ]
