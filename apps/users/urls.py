3c6d9bc1275aa90ba6b38be00650c11264f80251
yasir1brahim <yasir.ibrahim@gmail.com>
2025-03-24 16:28:04 +0500

    Removed unused web view of profile app

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -8,7 +8,5 @@ urlpatterns = [
     path("profile/", views.profile_api, name="user_profile_api"),
     
     # Web views
-    path("web/profile/", views.profile, name="user_profile"),
     path("profile/upload-image/", views.upload_profile_image, name="upload_profile_image"),
-    path('media/profile-pictures/<str:image_name>/', views.get_resized_avatar, name='get_avatar'),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -18,51 +18,13 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .serializers import CustomUserSerializer
 
-from .forms import CustomUserChangeForm, UploadAvatarForm
+from .forms import  UploadAvatarForm
 from .helpers import require_email_confirmation, user_has_confirmed_email_address
 from .models import CustomUser
 from PIL import Image
 import os
 import logging as log
 from django.views.decorators.csrf import csrf_exempt
-
-@login_required
-def profile(request):
-    if request.method == "POST":
-        form = CustomUserChangeForm(request.POST, instance=request.user)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user_before_update = CustomUser.objects.get(pk=user.pk)
-            need_to_confirm_email = (
-                user_before_update.email != user.email
-                and require_email_confirmation()
-                and not user_has_confirmed_email_address(user, user.email)
-            )
-            if need_to_confirm_email:
-                # don't change it but instead send a confirmation email
-                # email will be changed by signal when confirmed
-                new_email = user.email
-                send_email_confirmation(request, user, signup=False, email=new_email)
-                user.email = user_before_update.email
-                # recreate the form to avoid populating the previous email in the returned page
-                form = CustomUserChangeForm(instance=user)
-            user.save()
-            messages.success(request, _("Profile successfully saved."))
-    else:
-        form = CustomUserChangeForm(instance=request.user)
-    return render(
-        request,
-        "account/profile.html",
-        {
-            "form": form,
-            "active_tab": "profile",
-            "page_title": _("Profile"),
-            "api_keys": request.user.api_keys.filter(revoked=False),
-            "social_accounts": SocialAccount.objects.filter(user=request.user),
-            "user_has_valid_totp_device": user_has_valid_totp_device(request.user),
-        },
-    )
-
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
@@ -88,72 +50,6 @@ def upload_profile_image(request):
             {"errors": readable_errors}, 
             status=status.HTTP_400_BAD_REQUEST
         )
-
-@login_required
-def get_resized_avatar(request, image_name):
-    try:
-        original_image_path = os.path.join(settings.MEDIA_ROOT, 'profile-pictures', image_name)
-        if not os.path.exists(original_image_path):
-            raise Http404("Avatar not found.")
-        
-        query_params = request.GET
-        width = query_params.get('w')
-        height = query_params.get('h')
-        
-        base, ext = os.path.splitext(image_name)
-
-        resized_image_path = original_image_path  # Fallback to the original
- 
-        if width is not None or height is not None:
-            # Setup target dimensions
-            target_width = int(width) if width else None
-            target_height = int(height) if height else None
-            
-            resized_image_filename = f"{base}_{target_width if target_width else 'auto'}x{target_height if target_height else 'auto'}{ext}"
-            resized_image_path = os.path.join(settings.MEDIA_ROOT, 'profile-pictures', resized_image_filename)
-            if os.path.exists(resized_image_path):
-                with open(resized_image_path, 'rb') as f:
-                    return HttpResponse(f.read(), content_type="image/jpeg")
-
-            with Image.open(original_image_path) as img:
-                original_width, original_height = img.size
-                
-                # Determine target dimensions for square resizing if only one dimension is given
-                if target_width is None and target_height is None:
-                    raise ValueError("Must provide either width or height.")
-
-                if target_width is None:
-                    target_width = target_height
-                elif target_height is None:
-                    target_height = target_width
-
-                # Calculate resize ratios
-                width_ratio = target_width / original_width
-                height_ratio = target_height / original_height
-
-                if width_ratio > height_ratio:
-                    new_width = target_width
-                    new_height = int(original_height * width_ratio)
-                    img = img.resize((new_width, new_height), Image.LANCZOS)
-                    crop_top_bottom = (new_height - target_height) // 2
-                    img = img.crop((0, crop_top_bottom, new_width, crop_top_bottom + target_height))
-                else:
-                    new_height = target_height
-                    new_width = int(original_width * height_ratio)
-                    img = img.resize((new_width, new_height), Image.LANCZOS)
-                    crop_left_right = (new_width - target_width) // 2
-                    img = img.crop((crop_left_right, 0, crop_left_right + target_width, new_height))
-
-                if img.mode == 'RGBA':
-                    img = img.convert('RGB')
-
-                img.save(resized_image_path, format='JPEG')
-
-        with open(resized_image_path, 'rb') as f:
-            return HttpResponse(f.read(), content_type="image/jpeg")
-    
-    except Exception as e:
-        return HttpResponse(status=500)
 
 @api_view(['GET', 'POST', 'PATCH', 'DELETE'])
 @permission_classes([IsAuthenticated])
