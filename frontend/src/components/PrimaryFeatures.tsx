b4e822438e8303e8d24574b1012411dcbbcb4148
John Swope <john@johnswope.com>
2025-03-26 17:06:00 -0400

    fix: Homepage Update 2

--- frontend/src/components/PrimaryFeatures.tsx
+++ frontend/src/components/PrimaryFeatures.tsx
@@ -12,7 +12,7 @@ const features = [
   {
     title: 'Build',
     description:
-      "Build apps using the drag and drop editor without touching any code. Create apps that leverage AI to accelerate course development processes, scale your feedback, or automate assessment. All while protecting your voice and your pedagogy.",
+      "Build apps using the drag and drop editor without touching any code. Create apps that leverage AI to accelerate course development processes, scale your feedback, or automate assessment. All while protecting instructor voice and pedagogy.",
     image: '/img/homepage/build.gif',
   },
   {
