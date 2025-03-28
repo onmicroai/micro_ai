106a64eaa2ead79f1c936c8329ed616c28c21b69
brnikita <branikita@gmail.com>
2025-03-28 19:10:32 +0300

    feat: #148 add subscription field to SubscriptionConfiguration migration for enhanced subscription management

--- backend/apps/subscriptions/migrations/0004_subscriptionconfiguration.py
+++ backend/apps/subscriptions/migrations/0004_subscriptionconfiguration.py
@@ -16,6 +16,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('max_apps', models.IntegerField(help_text='Maximum number of apps allowed for this subscription')),
+                ('subscription', models.CharField(blank=True, max_length=255, null=True)),
             ],
         ),
     ]
