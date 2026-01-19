4b1d6c037ea3a771cd2e42955be37b9a2f30d2ff
ktr <pavlobutynez@gmail.com>
2026-01-19 18:45:13 +0200

    add smooth scrolling to SelectContent component

--- frontend/src/app/(authenticated)/app/(pages)/edit/[id]/components/ui/select.tsx
+++ frontend/src/app/(authenticated)/app/(pages)/edit/[id]/components/ui/select.tsx
@@ -86,6 +86,7 @@ const SelectContent = React.forwardRef<
       <SelectScrollUpButton />
       <SelectPrimitive.Viewport
         className={cn(
+          "p-1 scroll-smooth",
           "p-1",
           position === "popper" &&
             "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]"
