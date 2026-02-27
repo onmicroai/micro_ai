c0bbe3d97c6cd149d4539fc51ddf738542df01b0
ishandry <andriy.ishchukk@gmail.com>
2026-02-27 09:16:12 +0100

    adjust hover over items from select

--- frontend/src/app/(authenticated)/app/(pages)/edit/[id]/components/ui/select.tsx
+++ frontend/src/app/(authenticated)/app/(pages)/edit/[id]/components/ui/select.tsx
@@ -119,7 +119,7 @@ const SelectItem = React.forwardRef<
   <SelectPrimitive.Item
     ref={ref}
     className={cn(
-      "relative flex w-full cursor-default select-none items-center justify-between rounded-sm py-1.5 pl-2 pr-2 text-sm outline-none",
+      "relative flex w-full cursor-pointer select-none items-center justify-between rounded-sm py-1.5 pl-2 pr-2 text-sm outline-none",
       "focus:bg-primary/10 focus:text-primary-700",
       "hover:bg-primary/10 hover:text-primary-700",
       "data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
