11ae62e5a1db30ee60358aae96229b2ffb2e5e94
Emil-Khalus <emil.khalus7@gmail.com>
2026-03-31 18:46:40 +0200

    Add new icon components

---
+++ frontend/src/components/icons/CoinsSquareIcon.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const CoinsSquareIcon = () => {
+  return (
+    <svg
+      width="40"
+      height="40"
+      viewBox="0 0 40 40"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="40" height="40" rx="4" fill="#E1E3FF" fill-opacity="0.5" />
+      <path
+        d="M21.744 25.736C21.4438 26.7286 20.8915 27.6264 20.1409 28.342C19.3903 29.0575 18.4671 29.5663 17.4612 29.8187C16.4554 30.071 15.4013 30.0584 14.4018 29.7821C13.4024 29.5057 12.4915 28.975 11.7583 28.2417C11.025 27.5084 10.4943 26.5976 10.2179 25.5981C9.94155 24.5986 9.92895 23.5446 10.1813 22.5388C10.4337 21.5329 10.9425 20.6097 11.658 19.8591C12.3736 19.1085 13.2714 18.5562 14.264 18.256"
+        stroke="#5963E8"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M24 22C27.3137 22 30 19.3137 30 16C30 12.6863 27.3137 10 24 10C20.6863 10 18 12.6863 18 16C18 19.3137 20.6863 22 24 22Z"
+        stroke="#5963E8"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default CoinsSquareIcon;
